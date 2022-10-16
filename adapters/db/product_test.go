1b2feafbccf48e19fef9d07b9f871eab65d37585
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-16 18:50:04 -0300

    test: preparando testes de banco de dados

---
+++ adapters/db/product_test.go
@@ -0,0 +1,32 @@
+package db
+
+var Db *sql.Db
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products(
+						"id" string,
+						"name" string,
+						"price" float,
+						"status" string
+						);`
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `insert into product values("abc", "Product Test", 0, "disabled");`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
